a5262f5bef413bbc6f4f658a3f2622301919d7f0
Julien Tomezach <julien.tomezach@ensta-bretagne.org>
2020-08-09 20:00:16 +0200

    up

--- src/App.js
+++ src/App.js
@@ -8,13 +8,15 @@ function App() {
 
     <div className="App">
       <div className="Sidebar">
+      <div className="SidebarContent">
         <h3 >Menu</h3>
         <div href="#">Link 1</div>
         <div href="#">Link 2</div>
         <div href="#">Link 3</div>
       </div>
+      </div>
 
-      <div style={{marginLeft:'25%'}}>
+      <div className="LeftScreen">
 
           <div class="Header">
            <div> amount_for </div>
@@ -22,9 +24,11 @@ function App() {
 
 
           <div class="Content">
+          <div class="CodeBox">
           <h2>Sidebar Navigation Example</h2>
           <p>The sidebar with is set with "style="width:25%".</p>
           <p>The left margin of the page content is set to the same value.</p>
+          </div>
           </div>
 
       </div>
