5d2835729663a8e9927a135cd248151f6b1e9629
Julien Tomezach <julien.tomezach@ensta-bretagne.org>
2020-08-10 10:36:07 +0200

    dont understand but ok

--- src/App.js
+++ src/App.js
@@ -1,32 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+const axios_base = require('axios').default;
 
 function App() {
+
+   const [textCode, setTextCode] = useState("The code is \nloading...");
+   const axios = axios_base.create({
+                          baseURL: 'http://localhost:3000/',
+                        });
+
+   useEffect(() => {
+    let fetch = async () => {
+    try {
+      const response = await axios.get('/code');
+      setTextCode(response.data)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+      fetch();
+  });
+
   return (
 
 
     <div className="App">
       <div className="Sidebar">
       <div className="SidebarContent">
-        <h5 className="UserName"><span class="material-icons">person</span><span className="ItemText">Julien</span></h5>
-        <div href="#" className="SelectedItem"><span class="material-icons">text_snippet</span><span className="ItemText">amount_for</span></div>
+        <h5 className="UserName"><span className="material-icons">person</span><span className="ItemText">Julien</span></h5>
+        <div href="#" className="SelectedItem"><span className="material-icons">text_snippet</span><span className="ItemText">amount_for</span></div>
       </div>
       </div>
 
       <div className="RightScreen">
 
-          <div className="Header">
-           <div >amount_for </div>
-          </div>
+          {/*<div className="Header">
+
+          </div>*/}
 
 
           <div className="Content">
           <div className="CodeBoxParent">
           <div className="CodeBox">
-          <h2>Sidebar Navigation Example</h2>
-          <p>The sidebar with is set with "style="width:25%".</p>
-          <p>The left margin of the page content is set to the same value.</p>
+          {textCode}
           </div>
           </div>
           </div>
