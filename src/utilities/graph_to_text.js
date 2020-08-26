b6730837566eaad92557adc6c25a0223fbc091f9
Julien Tomezach <julien.tomezach@ensta-bretagne.org>
2020-08-26 18:01:37 +0200

    up

--- src/utilities/graph_to_text.js
+++ src/utilities/graph_to_text.js
@@ -16,6 +16,11 @@ const FUNCTION_CALL = 'FunctionCall'
 const COMMENT = 'Comment'
 const OPERATION = 'Operation'
 const INDEX = 'Index'
+
+
+let get_tabs = (n) => {
+    return _.sumBy(Array(n), o => "\t") || ""
+}
 // special case of the root elements
 let graph_to_text = (graph_input) => {
 
@@ -26,20 +31,24 @@ let graph_to_text = (graph_input) => {
     if(keys.length >1) throw Error(`graph_to_text: several keys for an element of the graph ${keys.join('')}`)
     let main_key = keys[0]
 
-    // first add the comments
-    if('comments' in element[main_key]){
+    let value = element[main_key]
+    // set the offset
+    let offset =  get_tabs(value.level)
+    // let offset =  get_tabs(0)
+    let line_break = '</br>' + offset
+    // then add the comments
+    if('comments' in value){
       element[main_key].comments.map(comment => {
-          textCode +=  SPAN("# "+comment, COMMENT) + '<br/>'
+          textCode += offset + SPAN("# "+comment, COMMENT) + '<br/>'
       })
     }
 
-    let value = Object.values(element)[0]
     if(!('formulas' in value)) throw Error('graph_to_text: "formulas" not in element')
     let formula = value.formulas
     // do something with the name of formula, the parameters
-    let parameters = value.parameters.map(param =>  graph_to_text_aux(param, PARAM)).join(", ")
-    let line_break = formula.definitions ? "\n" : ""
-    textCode += SPAN(main_key, FUNCTION) + "("+ parameters+ ")"+ " = " + line_break + graph_to_text_aux(formula) + '<br/>' + '<br/>'
+    let parameters = value.parameters.map(param =>  graph_to_text_aux(param, line_break, PARAM)).join(", ")
+    let line_break_or_not = formula.definitions ? line_break : ""
+    textCode += offset + SPAN(main_key, FUNCTION) + "("+ parameters+ ")"+ " = " + line_break_or_not + graph_to_text_aux(formula, line_break) + '<br/>' + '<br/>'
 
 
   })
@@ -55,46 +64,47 @@ const SPAN = (content, className) => {
  return first + content + last
 }
 
-let graph_to_text_aux = (graph_input, type=null) => {
+// why add line break everywhere ???
+let graph_to_text_aux = (graph_input, line_break, type=null) => {
   if('definitions' in graph_input){
     let definitions = graph_input.definitions
-    return definitions.map(([condition, formula]) => graph_to_text_aux(condition)+" | "+graph_to_text_aux(formula)).join('\n')
+    return definitions.map(([condition, formula]) => graph_to_text_aux(condition, line_break)+" | "+graph_to_text_aux(formula, line_break)).join(line_break)
   }else {
     if('sum_on' in graph_input){
       let [iteree, formula] = graph_input.sum_on
       // sum_on(owner.lots) {
-      return SPAN("sum_on",FUNCTION_CALL)+"("+ graph_to_text_aux(iteree)+")"+" { "+ graph_to_text_aux(formula) + " }"
+      return SPAN("sum_on",FUNCTION_CALL)+"("+ graph_to_text_aux(iteree, line_break)+")"+" { "+ graph_to_text_aux(formula, line_break) + " }"
     }else if('.' in graph_input){
-        return graph_input['.'].map(sub_element => graph_to_text_aux(sub_element)).join(SPAN('.', OPERATION))
+        return graph_input['.'].map(sub_element => graph_to_text_aux(sub_element, line_break)).join(SPAN('.', OPERATION))
     }else if('*' in graph_input){
-        return graph_input['*'].map(sub_element => graph_to_text_aux(sub_element)).join(SPAN(' * ', OPERATION))
+        return graph_input['*'].map(sub_element => graph_to_text_aux(sub_element, line_break)).join(SPAN(' * ', OPERATION))
     }else if('/' in graph_input){
-        return graph_input['/'].map(sub_element => graph_to_text_aux(sub_element)).join(SPAN(' / ', OPERATION))
+        return graph_input['/'].map(sub_element => graph_to_text_aux(sub_element, line_break)).join(SPAN(' / ', OPERATION))
     }else if('-' in graph_input){
-        return graph_input['-'].map(sub_element => graph_to_text_aux(sub_element)).join(SPAN(' - ', OPERATION))
+        return graph_input['-'].map(sub_element => graph_to_text_aux(sub_element, line_break)).join(SPAN(' - ', OPERATION))
     }else if('+' in graph_input){
-        return graph_input['+'].map(sub_element => graph_to_text_aux(sub_element)).join(SPAN(' + ', OPERATION))
+        return graph_input['+'].map(sub_element => graph_to_text_aux(sub_element, line_break)).join(SPAN(' + ', OPERATION))
     }else if('??' in graph_input){
-        return graph_input['??'].map(sub_element => graph_to_text_aux(sub_element)).join(SPAN(' ?? ', OPERATION))
+        return graph_input['??'].map(sub_element => graph_to_text_aux(sub_element, line_break)).join(SPAN(' ?? ', OPERATION))
     }else if('[]' in graph_input){
-        return graph_to_text_aux(graph_input['[]'][0])+"["+SPAN(graph_input['[]'][1], INDEX)+"]"
+        return graph_to_text_aux(graph_input['[]'][0], line_break)+"["+SPAN(graph_input['[]'][1], INDEX)+"]"
     }else if('==' in graph_input){
-        return graph_input['=='].map(sub_element => graph_to_text_aux(sub_element)).join(SPAN(' == ', OPERATION))
+        return graph_input['=='].map(sub_element => graph_to_text_aux(sub_element, line_break)).join(SPAN(' == ', OPERATION))
     }else if('!=' in graph_input){
-        return graph_input['!='].map(sub_element => graph_to_text_aux(sub_element)).join(SPAN(' != ', OPERATION))
+        return graph_input['!='].map(sub_element => graph_to_text_aux(sub_element, line_break)).join(SPAN(' != ', OPERATION))
     }else if('()' in graph_input){
-        return SPAN(' ( ', OPERATION) + graph_to_text_aux(graph_input['()']) + SPAN(' ) ', OPERATION)
+        return SPAN(' ( ', OPERATION) + graph_to_text_aux(graph_input['()'], line_break) + SPAN(' ) ', OPERATION)
     }else if('where' in graph_input){
       // it is binary operator too like +, - ...
         let element = graph_input['where']
-        return graph_to_text_aux(element[0])+" where "+ graph_to_text_aux(element[1])
+        return graph_to_text_aux(element[0], line_break)+" where "+ graph_to_text_aux(element[1], line_break)
     }else if('name' in graph_input && !('parameters' in graph_input)) {
 
       let is_number = !isNaN(graph_input.name)
       let new_type = is_number ? INDEX: type
       return SPAN(graph_input.name, new_type)
     }else if('name' in graph_input && 'parameters' in graph_input) {
-      let parameters = graph_input.parameters.map(param =>  graph_to_text_aux(param)).join(", ")
+      let parameters = graph_input.parameters.map(param =>  graph_to_text_aux(param, line_break)).join(", ")
       return SPAN(graph_input.name, FUNCTION_CALL)+"("+parameters+")"
     }
     throw Error(`graph_to_text: we dont knwo that key word in formula: ${Object.keys(graph_input)}`)
