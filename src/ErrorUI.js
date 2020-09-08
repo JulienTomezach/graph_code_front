d7d66f5b140c0a3b3c30c834f2779448db5f9e8b
Julien Tomezach <julien.tomezach@ensta-bretagne.org>
2020-09-08 12:14:09 +0200

    up

--- src/ErrorUI.js
+++ src/ErrorUI.js
@@ -4,10 +4,11 @@ import  styles from './ErrorUI.module.css';
 function ErrorUI(props) {
 
   return <div className= {styles.ErrorUI}>
-      {<span className={styles.Close}>X</span>}
+      <span className={styles.NotifType}><span className="material-icons">error_outline</span></span>
+      <span className={styles.Close}>X</span>
       <span className={styles.ContentParent}>
         <span className={styles.Content}>
-        Nulla facilisi. Phasellus pretium enim metus, sit amet tincidunt ligula semper mattis. Maecenas magna neque, congue ut dignissim consectetur, feugiat vel quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam luctus accumsan metus. Cras mattis laoreet varius. Suspendisse vel tempus ante. Proin risus nulla, ullamcorper at finibus quis, feugiat eu dolor. Nulla placerat sapien ex, in consequat felis ultrices eget.
+        What have you done ...
 
         </span>
       </span>
