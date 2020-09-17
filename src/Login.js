e2d417c502f7c5a209e2d6cb3847c6a0706e13bc
Julien Tomezach <julien.tomezach@ensta-bretagne.org>
2020-09-17 22:31:32 +0200

    up

--- src/Login.js
+++ src/Login.js
@@ -4,6 +4,27 @@ import  stylesLanding from './Landing.module.css';
 
 
 function Login(props) {
+
+	const sendLogin = () => {
+		let username = document.getElementById("username_input").innerText
+		let password = document.getElementById("password_input").innerText
+		password
+	    let response = await axios.post(`files`, {name: filename});
+	    if(response.status === 200){
+	      await fetchFiles()
+	      setAddingFile(v => !v)
+	    }
+	}
+
+	const errorMessageComponent =  () => {
+		// put that in back
+		let badPassword = password.length < 8 || password.match(/[0-9]+/) === null || password.match(/[^A-Za-z0-9]/) === null
+		let msg = ''
+		if(badPassword) msg += 'The password must be at least 8 characters, with one or more numbers and special characters'
+			// check no space in username
+		return (<div className={styles.ErrorMessage}>{errorMessage}</div>)
+	}
+
 	return (
 		<div className={stylesLanding.LandingParent}>
 			<div className={stylesLanding.Landing }>
@@ -11,7 +32,8 @@ function Login(props) {
 					<div className={`${styles.Logo} ${styles.Element}`}>&#931;xplicit</div>
 					<input placeholder="Username" id="username_input" type="text" className={`Input ${styles.Element}`} />
 					<input placeholder="Password" id="password_input" type="password" className={`Input ${styles.Element}`} />
-					<div className={`${styles.GoButton}`}>Go</div>
+
+					<div onClick={sendLogin} className={`${styles.GoButton}`}>Go</div>
 				</div>
 			</div>
 		</div>
