10b471543b47a6384a9e48b7633b7aa77d81525a
Julien Tomezach <julien.tomezach@ensta-bretagne.org>
2020-09-17 16:37:08 +0200

    up

--- src/Login.js
+++ src/Login.js
@@ -5,11 +5,17 @@ import  styles from './Login.module.css';
 function Login(props) {
 
 	return <div className={styles.Login}>
+
 		<div className={styles.Header}>
 				<div className={styles.HeaderTitle}>
 					Explicit
 				</div>
-				<div className={styles.HeaderSubTitle}>
+		</div>
+		<div className={styles.HeaderSpace}>
+		</div>
+		
+		<div className={styles.MainPart}>
+				<div className={styles.SubTitle}>
                     The place for your Business and Product Logic 
                  </div>   
               
@@ -20,10 +26,12 @@ function Login(props) {
 		</div>
 			
 		</div>
+
+
 		<div className={styles.Features}>
 			<div className={styles.Feature}>
 			  <div className={styles.FeatureTitle}>
-			  		Simple
+			  		<span className={styles.ActualFeatureTitle}>Simple</span>
 			  </div>
 			  <div className={styles.FeatureContent}>
 			   	No Production Code constraints like performance, security or Front-Back communication. The business logic is as simple as it can be.
@@ -31,21 +39,24 @@ function Login(props) {
 			</div>
 			<div className={styles.Feature}>
 			  <div className={styles.FeatureTitle}>
-			  		Still Code
+			  		<span className={styles.ActualFeatureTitle}>Still Code</span>
 			  </div>
 			  <div className={styles.FeatureContent}>
                So computer can run it, check consistency, properties of the system and more.
 			   </div>
 			</div>
-			<div className={styles.Feature}>
+			
+              
+		</div>
+		<div className={styles.Features}>
+		<div className={styles.Feature}>
 			  <div className={styles.FeatureTitle}>
-			  		Flexible
+			  		<span className={styles.ActualFeatureTitle}>Flexible</span>
 			  </div>
 			  <div className={styles.FeatureContent}>
                	Go in as much or as little detail you want. The code always runs and gives you valuable feedbacks.
 			   </div>
 			</div>
-              
 		</div>
 	</div>
 }
