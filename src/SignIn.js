ddc8138c21b5a7f4d97316253719eeb1d42a8fea
Julien Tomezach <julien.tomezach@ensta-bretagne.org>
2020-09-27 22:27:41 +0200

    up

--- src/SignIn.js
+++ src/SignIn.js
@@ -14,6 +14,9 @@ function SignIn(props) {
 
 	 let [subscribed, setSubscribed] = useState(false)
 	 let [displayErrorDesc, setDisplayErrorDesc] = useState(false)
+	 let [badForm, setBadForm] = useState(false)
+	 let [badEmail, setBadEmail] = useState(false)
+	 const EMAIL_REGEX = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 			// put that in back
 	const errorMessageComponent =  (password, username) => {
@@ -30,25 +33,46 @@ function SignIn(props) {
 		// email, description
 		let email = document.getElementById("email_input").value
 		let description = document.getElementById("text_beta_user").value
-		if(description.length === 0){
+		let username = document.getElementById("name_input").value
+		if(description.length === 0 ){
 			setDisplayErrorDesc(true)
+			return
 		}else{
 			setDisplayErrorDesc(false)
 		}
-		if(email.length > 0 && description.length > 0){
-			    let response = await axios.put(`sign_in`, {email, description});
+		if(email.length > 0 && description.length > 0 && username.length > 0 ){
+				setBadForm(false)
+				if(email.match(EMAIL_REGEX) === null){
+					setBadEmail(true)
+					return
+				}
+				setBadEmail(false)
+			    let response = await axios.put(`sign_in`, {email, description, name: username});
 			    if(response.status === 200){
 			    		setSubscribed(true)
+			    		setTimeout(() => (window.location.href = '/landing'), 3000);
 			    }else{
 			    	// let {message} = response.data
 			    	// do something
 			    }
+		}else{
+			setBadForm(true)
 		}
 	}
 	let subscribedComp = () => {
 		return subscribed ? (<div style={{marginTop: '10px' }}>
 			You are all set. We sent you an email.
 		</div>) : null
+	}
+
+	let genericError = () => {
+		if(badEmail) return (<div style={{color: 'red', marginTop: '3px'}}>
+			Your email is not correct
+		</div>)
+		else if(badForm) return (<div style={{color: 'red', marginTop: '3px'}}>
+			you need to fill in the form (email, username and the note)
+		</div>)
+		 else return erroDescComp()
 	}
 
 	let erroDescComp = () => {
@@ -61,11 +85,10 @@ function SignIn(props) {
 	let question0 = "Some (optional) questions:"
 	let question1 = "For what kind of business logic you would like to use Explicit ? (roughly)"
 	let question2 = "What is the size of your company/ dev team ?"
-	let question3 = "Did you ever feel that your business logic could be simplified but it was hard to do so ?"
 	let question4 = "Did you try the live demo ?"
 
-					// <input placeholder="Password" id="password_input" type="password" className={`Input`} />
-					// <div className={`${styles.TextParent}`}>
+	// add a name input here.
+	//<input className={`Input`} style={{width: '250px'}} placeholder="username" id="name_input" type="text"  />
 	return (
 		<div className={stylesLanding.LandingParent}>
 			<div className={stylesLanding.Landing }>
@@ -77,18 +100,18 @@ function SignIn(props) {
 		</div>
 				<div className={styles.Card}>
 						<input className={`Input`} style={{width: '250px'}} placeholder="Professional email (ideally)" id="email_input" type="text"  />
+						<input className={`Input`} style={{width: '250px'}} placeholder="username" id="name_input" type="text"  />
 						<div> <span style={{fontSize: '15px', marginLeft: '3px',marginRight: '3px'}}>&#128584;</span> We won t send you emails except for app operations like checking identity etc.. </div>
 						<div > <span style={{fontSize: '25px', marginLeft: '3px',marginRight: '3px'}}>&#129488;</span> {question0}</div>
 						<div className={styles.Question} >{question1}</div>
 						<div className={styles.Question} >{question2}</div>
-						<div className={styles.Question} >{question3}</div>
 						<div className={styles.Question} >{question4}</div>
 						<textarea placeholder="Type here" className={styles.TextArea} id="text_beta_user" rows="20" cols="60"></textarea>
 					<div style={{marginTop: '10px' }}> Thank you ! </div>
 					<div onClick={sendSignIn} className={`${stylesLogin.GoButton}`} style={{marginTop: '10px' }}>Sign in</div>
 					{subscribedComp()}
 				</div>
-					{erroDescComp()}
+					{genericError()}
 			</div>
 		</div>
 		)
